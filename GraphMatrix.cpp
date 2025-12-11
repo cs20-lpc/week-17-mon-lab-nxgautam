c1d14cd7fd1a367b2f45b9db6ec4942f3c78a3fb
Nikesh <nxgautam0001@zonemail.clpccd.edu>
2025-12-10 21:46:45 -0800

    Finished Week 17 Mon Lab

--- GraphMatrix.cpp
+++ GraphMatrix.cpp
@@ -1,15 +1,48 @@
 #include "GraphMatrix.hpp"
 
-GraphMatrix::GraphMatrix(int numV, GType type) {
+GraphMatrix::GraphMatrix(int numV, GType type) : Graph(numV, 0, type){
     // TODO
+
+    adjMatrix = new bool*[v];
+    for(int i = 0; i < v; i++)
+    {
+        adjMatrix[i] = new bool[v];
+
+        for(int j = 0; j < v; j++)
+        {
+            adjMatrix[i][j] = false;
+        }
+    }
+
 }
 
 GraphMatrix::~GraphMatrix() {
     // TODO
+
+    if(adjMatrix != nullptr)
+    {
+        for(int i = 0; i < v; i++)
+        {
+            delete[] adjMatrix[i];
+        }
+
+        delete[] adjMatrix;
+
+    }
+
+
 }
 
 bool GraphMatrix::isEdge(int i, int j) const {
     // TODO
+
+    if(i < 0 || i >= v || j < 0 || j >= v)
+    {
+        return false;
+    }
+
+    return adjMatrix[i][j];
+
 }
 
 ostream& GraphMatrix::print(ostream& os) const {
@@ -38,4 +71,31 @@ ostream& GraphMatrix::print(ostream& os) const {
 
 void GraphMatrix::toggleEdge(int i, int j) {
     // TODO
+
+    if(i < 0 || i >= v || j < 0 || j >= v)
+    {
+        return;
+    }
+
+    if(adjMatrix[i][j])
+    {
+        adjMatrix[i][j] = false;
+        e--;
+
+
+        if(graphType == UNDIRECTED && i != j)
+        {
+            adjMatrix[j][i] = false;
+        }
+    }else
+    {
+        adjMatrix[i][j] = true;
+        e++;
+
+        if(graphType == UNDIRECTED && i != j)
+        {
+            adjMatrix[j][i] = true;
+        }
+    }
+
 }
